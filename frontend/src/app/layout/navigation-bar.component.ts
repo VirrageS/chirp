af6cbbf590a384e395a3ad5e682ecba462f0cbfe
Janusz Marcinkiewicz <virrages@gmail.com>
2016-10-18 19:24:30 +0200

    Change global font (Roboto -> Open Sans); Minor css cleanup

--- frontend/src/app/layout/navigation-bar.component.ts
+++ frontend/src/app/layout/navigation-bar.component.ts
@@ -8,12 +8,10 @@ import { Component } from '@angular/core';
       flex-direction: row;
       justify-content: space-between;
       align-items: center;
-
       height: 60px;
       margin-bottom: 30px;
-      border-bottom: 1px;
-
       padding: 0px 5vw;
+
       border-bottom: 1px solid #c9c9c9;
     }
 
@@ -22,15 +20,16 @@ import { Component } from '@angular/core';
       flex-direction: row;
       justify-content: space-between;
       align-items: center;
-
-      font-weight: 500;
-      color: #000000;
       width: 300px;
+
+      font-weight: 600;
+
+      color: #000000;
     }
 
     .menu__link {
-      color: inherit;
       text-decoration: none;
+      color: inherit;
     }
 
     .menu__link:hover, .menu__link--active {
