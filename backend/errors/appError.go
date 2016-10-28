d81f2bea266ae367e0786e23c32d8b77a7203386
Michal Pryt <misi3kd@gmail.com>
2016-10-28 21:11:46 +0200

    Add custom error class

---
+++ backend/errors/appError.go
@@ -0,0 +1,17 @@
+package errors
+
+import "github.com/kataras/go-errors"
+
+type AppError struct {
+	Code    int
+	Err 	error
+}
+
+var UnexpectedError = &AppError{
+	Code: 500,
+	Err: errors.New("Unexpected error."),
+}
+
+func (error AppError) Error() string {
+	return error.Err.Error()
+}
