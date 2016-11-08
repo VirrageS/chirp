40c69d9ca96c799fd2a952bf259df3ae2f005b1e
Janusz Marcinkiewicz <virrages@gmail.com>
2016-11-08 10:26:44 +0100

    Improve UI

--- frontend/src/app/components/login.component.ts
+++ frontend/src/app/components/login.component.ts
@@ -5,39 +5,8 @@ import { User, AuthService } from '../shared';
 
 
 @Component({
-  template: `
-    <div class="container">
-      <h1>Login</h1>
-      <form (ngSubmit)="onSubmit()" #loginForm="ngForm">
-        <div class="form-group">
-          <label for="email">Email</label>
-          <input type="email" class="form-control" id="email"
-                 required
-                 [(ngModel)]="user.email" name="email"
-                 #email="ngModel"
-                 pattern="[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,6}" >
-        </div>
-        <div [hidden]="email.valid || email.pristine"
-             class="alert alert-danger">
-          Email is required
-        </div>
-
-        <div class="form-group">
-          <label for="password">Password</label>
-          <input type="password" class="form-control" id="password"
-                 required
-                 [(ngModel)]="user.password" name="password"
-                 #password="ngModel" >
-        </div>
-        <div [hidden]="password.valid || password.pristine"
-             class="alert alert-danger">
-          Password is required
-        </div>
-
-        <button type="submit" class="btn btn-default" [disabled]="!loginForm.form.valid">Submit</button>
-      </form>
-    </div>
-  `
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
 })
 export class LoginComponent {
   user: User
